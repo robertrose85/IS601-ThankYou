2b721ebd88515e03d651acad83d7a6efed969160
Bob Rose <rr637@njit.edu>
2018-10-28 21:44:04 -0400

    Task: Set up laravel, set up layout, includes, and pages. Include bootstrap code.

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+
+<main role="main">
+@yield('content')
+</main>
+
+@include('includes.jsfooter')
+@include('includes.footer')
+
+
+</body>
+</html>
