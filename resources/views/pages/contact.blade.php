7bb424b33994ae749018895932d9568a8f59941b
Bob Rose <rr637@njit.edu>
2018-10-28 22:49:19 -0400

    Fix: Padding / Margins

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="card">
+    <div class="card mt-3 pl-2 pr-2">
         <div class="card-title">
             <h1>Contact</h1>
             <p class="lead">Use form to contact site admin.</p>
