825a0d7bac40ec9dbb9c98548d19cb9cd519b515
Bob Rose <rr637@njit.edu>
2018-10-28 23:16:52 -0400

    Add: Pages Controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function home() {
+        return view('pages.index');
+    }
+    function about() {
+        return view('pages.about');
+    }
+    function contact() {
+        return view('pages.contact');
+    }
+
+
+
+}
